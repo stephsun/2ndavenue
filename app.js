917f60d7fb3e4353e98644688b0b1faabbd78377
Stephanie Sun <stephanie.sun@sungardas.com>
2015-11-10 16:39:05 -0800

    Served static files and tempaltes

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+'use strict';
+
 var express = require('express');
+var path = require('path');
+var hbs = require('hbs');
+
 var app = express();
 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'templates'));
+
+app.use(express.static('static'));
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.render('index', { title: 'Home', message: 'Hello there!'});
 });
 
 var server = app.listen(3000, function () {
